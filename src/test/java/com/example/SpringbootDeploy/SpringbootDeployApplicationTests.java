74dafb31b92c5650707360644737494ef21d8128
ChristianFlor <cdflor@outlook.com>
2022-09-13 21:07:50 -0500

    Add files

---
+++ src/test/java/com/example/SpringbootDeploy/SpringbootDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
